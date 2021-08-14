40c39a0ce04306aebed93c178d0307e516709014
kaouchi Mohamed <med_kaouchi@hotmail.com>
2021-08-14 13:32:31 +0200

    no comment

--- MTC_WebServerCore/Controllers/ProductAdminController.cs
+++ MTC_WebServerCore/Controllers/ProductAdminController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MTCrepository.TDSrepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,19 @@ namespace MTC_WebServerCore.Controllers
 {
     public class ProductAdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<HomeController> _logger;
+        private readonly IApplicationRepository _repos;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, IApplicationRepository appRepos, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _logger = logger;
+            _repos = appRepos;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {var vm=_repos.
         }
     }
 }
