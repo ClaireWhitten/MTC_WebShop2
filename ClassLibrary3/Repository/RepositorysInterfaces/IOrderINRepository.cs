1617fc5941149f964c9b749560e4661e86898e15
Claire Whitten <claire.whitten@student.kdg.be>
2021-08-26 19:20:51 +0200

    Claire Supplier 26/8 7pm

--- ClassLibrary3/Repository/RepositorysInterfaces/IOrderINRepository.cs
+++ ClassLibrary3/Repository/RepositorysInterfaces/IOrderINRepository.cs
@@ -3,10 +3,13 @@ using MTCrepository.TDSrepository;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MTCrepository.Repository
 {
     public interface IOrderINRepository : ITDSrepositoryAsync<OrderIN>
     {
+
+        Task<TSDreposResultIenumerable<OrderIN>> GetOrderInsWithOrderLines(int id);
     }
 }
--- MTC_WebServerCore/wwwroot/js/site.js
+++ MTC_WebServerCore/wwwroot/js/site.js
@@ -25,13 +25,21 @@ function confirmReplenishStock() {
     //loops over each row  and fires the function
     orders.each(function () {
         //Gets all the descendants of row which are checked
-        var selectedCheckBox = $(this).find(":checked");
+        var selectedCheckBox = $(this).find("input.checkBox");
+        console.log("Selected = " + selectedCheckBox.length);
+        console.log("Selected = " + selectedCheckBox[0].checked);
+       
         
         //if checked descendant found (length not 0) selected save the values into a new object(EAN, name, amount and supplier)
-        if (selectedCheckBox.length > 0) {
+        if (selectedCheckBox[0].checked) {
+
             var EAN = $(this).attr("id");
             var name = $(this).children()[2].innerText;
-         
+            var RecommendedUnitPrice = $(this).children()[3].innerText;
+            var CountInStock = $(this).children()[4].innerText;
+            var MinStock = $(this).children()[5].innerText;
+            var MaxStock = $(this).children()[6].innerText;
+
             var amountToOrder = $(this).find(".amountToOrder").eq(0).val();
             var supplier = $(this).find("option:selected").val();
             //properties of object should match with vm property names
@@ -39,6 +47,10 @@ function confirmReplenishStock() {
                 product: {
                     EAN: EAN,
                     name: name,
+                    RecommendedUnitPrice: RecommendedUnitPrice,
+                    CountInStock: CountInStock,
+                    MinStock: MinStock,
+                    MaxStock: MaxStock
                 },
                 amountToOrder: amountToOrder,
                 supplierID: supplier
@@ -48,12 +60,12 @@ function confirmReplenishStock() {
         }
     });
 
-    //console.log(arrayOfOrders);
+    console.log(arrayOfOrders);
     //post request to action containing array of orders
     $.post('/OrderIn/ReplenishStock',
         { orders: arrayOfOrders },
         function (data, status) {
-            console.log('status:' + status + 'data:' + data)
+            window.location = "/OrderIn/OverviewOrders";
         }
     )
 
